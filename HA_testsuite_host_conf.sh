ff6478711c5f22415f93d33b3aa10fb09608efa7
aginies <aginies@suse.com>
2016-06-23 19:05:55 +0200

    add missing call to functions...

--- HA_testsuite_host_conf.sh
+++ HA_testsuite_host_conf.sh
@@ -10,6 +10,14 @@
 #zypper addrepo "iso:/?iso=SLE-12-SP2-Server-DVD-x86_64-Buildxxxx-Media1.iso&url=nfs://10.0.1.99/volume1/install/ISO/SP2devel/" ISOSLE
 #zypper addrepo "iso:/?iso=SLE-12-SP2-HA-DVD-x86_64-Buildxxxx-Media1.iso&url=nfs://10.0.1.99/volume1/install/ISO/SP2devel/" ISOHA
 
+if [ -f `pwd`/functions ] ; then
+    . `pwd`/functions
+else
+    echo "! need functions in current path; Exiting"; exit 1
+fi
+check_config_file
+
+
 # Install all needed Hypervisors tools
 install_virtualization_stack() {
     echo "############ START install_virtualization_stack #############"
