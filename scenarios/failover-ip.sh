ad22011d360d2f396959a09b0a3a5f6ed5d02bbe
aginies <aginies@suse.com>
2016-06-30 17:12:07 +0200

    fix indentation, wait after delete before clear

--- scenarios/failover-ip.sh
+++ scenarios/failover-ip.sh
@@ -19,8 +19,8 @@ IPEND="222"
 CIBNAME="failoverip-cib"
 
 failover_ip_cib() {
-	echo "############ START failover_ip_cib"
-	exec_on_node ha1 "crm<<EOF
+    echo "############ START failover_ip_cib"
+    exec_on_node ha1 "crm<<EOF
 cib new ${CIBNAME}
 configure primitive failover-ip ocf:heartbeat:IPaddr2 params ip=${NETWORK}.${IPEND} cidr_netmask=32 op monitor interval=1s
 verify
@@ -32,43 +32,43 @@ EOF"
 }
 
 test_failoverip() {
-	echo "############ START test_failoverip"
-	exec_on_node ha1 "crm status"
+    echo "############ START test_failoverip"
+    exec_on_node ha1 "crm status"
 }
 
 delete_cib_resource() {
-	echo "############ START delete_cib_resource"
-	exec_on_node ha1 "crm cib list | grep ${CIBNAME}"
-	if [ $? -eq 0 ]; then
+    echo "############ START delete_cib_resource"
+    exec_on_node ha1 "crm cib list | grep ${CIBNAME}"
+    if [ $? -eq 0 ]; then
 	echo "- Deleting cib and resource ${CIBNAME}"
 	exec_on_node ha1 "crm<<EOF
 resource stop failover-ip
+EOF"
+echo "- Wait stop/clear/delete resource (10s)"
+       sleep 10
+       exec_on_node ha1 "crm<<EOF
 resource clear failover-ip
 configure delete failover-ip
-EOF"
-	echo "- Wait stop/clear/delete resource"
-	sleep 5
-	exec_on_node ha1 "crm<<EOF
 cib delete failoverip-cib
 verify
 configure commit
 exit
 EOF"
-	else
-		echo "- cib ${CIBNAME} doesnt exist "
-	fi
-	echo "- Show status"
-	exec_on_node ha1 "crm status"
+    else
+	echo "- cib ${CIBNAME} doesnt exist "
+    fi
+    echo "- Show status"
+    exec_on_node ha1 "crm status"
 }
 
 ping_virtual_ip() {
-	echo "############ START ping_virtual_ip"
-	ping -c 2 ${NETWORK}.${IPEND}
-	if [ $? -eq 0 ]; then
-		echo "- ping ${NETWORK}.${IPEND} OK"
-	else
-		echo "- ! ping ${NETWORK}.${IPEND} FAILED"
-	fi
+    echo "############ START ping_virtual_ip"
+    ping -c 2 ${NETWORK}.${IPEND}
+    if [ $? -eq 0 ]; then
+	echo "- ping ${NETWORK}.${IPEND} OK"
+    else
+	echo "- ! ping ${NETWORK}.${IPEND} FAILED"
+    fi
 }
 
 ##########################
