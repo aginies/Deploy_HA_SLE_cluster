2151bd8b50ac6f10f4b4927643c6b8158e0cee4b
aginies <aginies@suse.com>
2016-07-22 16:43:13 +0200

    use drbd_common

--- scenarios/drbd_ext3.sh
+++ scenarios/drbd_ext3.sh
@@ -3,135 +3,19 @@
 #
 #
 #########################################################
-## DRBD OSCF2
+## DRBD EXT3
 #########################################################
 
-if [ -f ../functions ] ; then
-    . ../functions
+if [ -f drbd_common ] ; then
+    . drbd_common
 else
-    echo "! functions file needed! ; Exiting"
+    echo "! functions file drbd_common needed! ; Exiting"
     exit 1
 fi
-check_load_config_file other
 
 # SOME VARS
-CIBNAME="drbd_ocfs2"
+CIBNAME="drbd_ext3"
 DRBDRESOURCE="ha2"
-DRBDDEV="/dev/drbd0"
-POOLDRBD="DRBD"
-TARGETVD="vdd"
-NODEA="ha1"
-NODEB="ha2"
-MNTTEST="/mnt/test"
-IPA=`grep ${NODEA} /var/lib/libvirt/dnsmasq/${NETWORKNAME}.hostsfile | cut -d , -f 2`
-IPB=`grep ${NODEB} /var/lib/libvirt/dnsmasq/${NETWORKNAME}.hostsfile | cut -d , -f 2`
-
-install_packages() {
-    echo "############ START finstall_packages"
-    pssh -h /etc/hanodes "zypper in -y drbd-kmp-default drbd drbd-utils"
-}
-
-drbd_ocfs2_cib() {
-    echo "############ START drbd_ocfs2_cib"
-    exec_on_node ${NODEA} "crm<<EOF
-cib new ${CIBNAME}
-verify
-cib use live
-cib commit ${CIBNAME}
-exit
-EOF"
-}
-
-disable_drbd() {
-    echo "############ START disable_drbd"
-    echo "- Disable drbd service on node"
-    exec_on_node ${NODEA} "systemctl disable drbd"
-    exec_on_node ${NODEB} "systemctl disable drbd"
-}
-
-enable_drbd() {
-    echo "############ START enable_drbd"
-    echo "- Enable drbd service on node"
-    exec_on_node ${NODEA} "systemctl enable drbd"
-    exec_on_node ${NODEB} "systemctl enable drbd"
-}
-
-stop_drbd() {
-    echo "############ START stop_drbd"
-    echo "- Stop drbd service on node"
-    exec_on_node ${NODEA} "systemctl stop drbd"
-    exec_on_node ${NODEB} "systemctl stop drbd"
-}
-
-start_drbd() {
-    echo "############ START start_drbd"
-    echo "- Start drbd service on node"
-    exec_on_node ${NODEA} "systemctl start drbd"
-    exec_on_node ${NODEB} "systemctl start drbd"
-}
-
-
-create_vol_drbd() {
-    echo "############ START create_vol_drbd"
-    echo "- Create volume for DRBD device"
-    virsh vol-create-as --pool ${POOLDRBD} --name ${POOLDRBD}A.qcow2 --format qcow2 --capacity 1G --allocation 1G --prealloc-metadata
-    virsh vol-create-as --pool ${POOLDRBD} --name ${POOLDRBD}B.qcow2 --format qcow2 --capacity 1G --allocation 1G --prealloc-metadata
-    virsh vol-list ${POOLDRBD} --details
-    virsh pool-refresh --pool ${POOLDRBD}
-}
-
-delete_vol_drbd() {
-    echo "############ START delete_vol_drbd"
-    virsh vol-delete --pool ${POOLDRBD} ${POOLDRBD}A.qcow2
-    virsh vol-delete --pool ${POOLDRBD} ${POOLDRBD}B.qcow2
-}
-
-delete_pool_drbd() {
-    if [ $# -ne 1 ]; then echo "- delete_pool_drbd need one arg: POOL_NAME" ; exit 1; fi
-    POOL="$1"
-    echo "############ START delete_pool_drbd"
-    virsh pool-destroy ${POOL}
-    virsh pool-undefine ${POOL}
-    echo "- Delete path to volume"
-    rm -rf ${STORAGEP}/DRBD
-}
-
-attach_disk_to_node() {
-    echo "############ START attach_disk_to_node"
-    echo "- Attach volume to node"
-    virsh attach-disk ${DISTRO}HA1 --live --cache none --type disk ${STORAGEP}/${POOLDRBD}/${POOLDRBD}A.qcow2 --target ${TARGETVD}
-    virsh attach-disk ${DISTRO}HA2 --live --cache none --type disk ${STORAGEP}/${POOLDRBD}/${POOLDRBD}B.qcow2 --target ${TARGETVD}
-}
-
-check_targetvd_on_node() {
-    # workaround as this is not possible to garantee target device on VM guest
-    if [ $# -ne 1 ]; then echo "- check_targetvd_on_node need one arg: NODE_NAME" ; exit 1; fi
-    NODE=$1
-    exec_on_node ${NODE} "ls -la /dev/${TARGETVD}"
-    if [ $? -eq 0 ]; then
-	return REALTARGETVD=${TARGETVD}
-    else
-	for LETTER in {e..z}; do
-	    #echo "- Testing /dev/vd${LETTER}"
-	    export REALTARGETVD="vd${LETTER}"
-	    exec_on_node ${NODE} "ls -la /dev/vd${LETTER}"
-	    if [ $? -eq 0 ]; then
-		#echo "- Switching target on HA node to vd${LETTER}"
-		return REALTARGETVD="vd${LETTER}"
-		break
-	    #else
-	    #echo "- Trying another letter..."
-	    fi
-	done
-    fi
-}
-
-detach_disk_from_node() {
-    echo "############ START detach_disk_from_node"
-    echo "- Detach volume from node"
-    virsh detach-disk ${DISTRO}HA1 ${TARGETVD}
-    virsh detach-disk ${DISTRO}HA2 ${TARGETVD}
-}
 
 create_drbd_resource() {
     echo "############ START create_drbd_resource"
@@ -192,12 +76,6 @@ format_ext3() {
     exec_on_node ${NODEA} "mkfs.ext3 -F ${DRBDDEV}"
 }
 
-format_ocfs2() {
-    # need DLM
-    echo "############ START format_ocfs2"
-    exec_on_node ${NODEA} "mkfs.ocfs2 ${DRBDDEV}"
-}
-
 umount_mnttest() {
     echo "############ START umount_mnttest"
     exec_on_node ${NODEA} "umount ${MNTTEST}"
@@ -255,24 +133,13 @@ back_to_begining() {
     exec_on_node ${NODEA} "csync2 -xv"
 }
 
-configure_resources() {
-    echo "############ START configure_resources"
-    echo "- dual primary DRBD"
-    exec_on_node ${NODEA} "crm<<EOF
-configure primitive resDRBD ocf:linbit:drbd params drbd_resource=${DRBDRESOURCE} operations \$id='resDRBD-operations op monitor interval='20' role='Master' timeout='20' op monitor interval='30' role='Slave' timeout='20' 
-configure ms msDRBD resDRBD configure meta resource-stickines='100' notify='true' master-max='2' interleave='true'
-EOF"
-    exec_on_node ${NODEB} "crm status"
-}
-
-
 ##########################
 ##########################
 ### MAIN
 ##########################
 ##########################
 
-echo "############ DRBD / OSCFS2 SCENARIO #############"
+echo "############ DRBD / EXT3 SCENARIO #############"
 echo "  !! WARNING !! "
 echo "  !! WARNING !! "
 echo
@@ -281,7 +148,6 @@ read
 read
 
 install_packages
-#drbd_ocfs2_cib
 stop_drbd
 umount_mnttest
 
@@ -291,13 +157,9 @@ create_vol_drbd
 attach_disk_to_node
 create_drbd_resource
 drbdconf_csync2
-#start_drbd
 finalize_DRBD_setup
 format_ext3
 check_primary_secondary
-
-#format_ocfs2
-#configure_resources
 
 # restore before runnning the test
 back_to_begining
