d7d0370fe2d8c49e28f3a25ad7145028c72cd371
aginies <aginies@suse.com>
2016-07-22 17:43:40 +0200

    start to fix ocsf2 script

--- scenarios/drbd_ocfs2.sh
+++ scenarios/drbd_ocfs2.sh
@@ -30,13 +30,23 @@ EOF"
 
 create_drbd_resource() {
     echo "############ START create_drbd_resource"
-    echo "- Create /etc/drbd.d/drbd.res file"
+    echo "- Create /etc/drbd.d/drbdo2.res file"
     check_targetvd_on_node ${NODEA} > /tmp/check_targetvd_on_node_${NODEA}
     REALTARGETVDA=`cat /tmp/check_targetvd_on_node_${NODEA} | tail -1 | awk -F "/dev/" '{print $2}'`
     check_targetvd_on_node ${NODEB} > /tmp/check_targetvd_on_node_${NODEB}
     REALTARGETVDB=`cat /tmp/check_targetvd_on_node_${NODEB} | tail -1 | awk -F "/dev/" '{print $2}'`
-    exec_on_node ${NODEA} "cat >/etc/drbd.d/drbd.res<<EOF
-resource drbd {
+    exec_on_node ${NODEA} "cat >/etc/drbd.d/drbdo2.res<<EOF
+resource drbdo2 {
+  startup {
+    become-primary-on both;
+  }
+  net {
+    ## allow-two-primaries;
+    after-sb-0pri discard-zero-changes;
+    after-sb-1pri discard-secondary;
+    after-sb-2pri disconnect;
+  }
+
     device ${DRBDDEV};
     meta-disk internal;
     on ${NODEA} {
@@ -47,6 +57,7 @@ resource drbd {
       address ${IPB}:7790;
       disk /dev/${REALTARGETVDB};
     }
+}
 EOF"
 }
 
@@ -67,25 +78,25 @@ c/csync2/csync2.cfg"
 finalize_DRBD_setup() {
     echo "############ START finalize_DRBD_setup"
     echo "- Initializes the metadata storage"
-    exec_on_node ${NODEA} "yes yes | drbdadm create-md drbd"
-    exec_on_node ${NODEB} "yes yes | drbdadm create-md drbd"
+    exec_on_node ${NODEA} "yes yes | drbdadm create-md drbdo2"
+    exec_on_node ${NODEB} "yes yes | drbdadm create-md drbdo2"
     echo "- Create the /dev/drbd"
-    exec_on_node ${NODEA} "drbdadm up drbd"
-    exec_on_node ${NODEB} "drbdadm up drbd"
+    exec_on_node ${NODEA} "drbdadm up drbdo2"
+    exec_on_node ${NODEB} "drbdadm up drbdo2"
     echo "- Create a new UUID to shorten the initial resynchronization of the DRBD resource"
-    exec_on_node ${NODEA} "drbdadm new-current-uuid drbd/0"
+    exec_on_node ${NODEA} "drbdadm new-current-uuid drbdo2/0"
     echo "- Make ${NODEA} primary"
-    exec_on_node ${NODEA} "drbdadm primary drbd"
+    exec_on_node ${NODEA} "drbdadm primary drbdo2"
     echo "- Check the DRBD status"
     exec_on_node ${NODEA} "cat /proc/drbd"
     echo "- Start the resynchronization process on your intended primary node"
-    exec_on_node ${NODEA} "drbdadm -- --overwrite-data-of-peer primary drbd"
+    exec_on_node ${NODEA} "drbdadm -- --overwrite-data-of-peer primary drbdo2"
 }
 
 format_ocfs2() {
     # need DLM
     echo "############ START format_ocfs2"
-    exec_on_node ${NODEA} "mkfs.ocfs2 --force --cluster-stack pcmk -L 'VMtesting' --cluster-name o2cb ${DRBDDEV}"
+    exec_on_node ${NODEA} "mkfs.ocfs2 --force --cluster-stack pcmk -L 'VMtesting' --cluster-name drbdo2 ${DRBDDEV}"
 }
 
 umount_mnttest() {
@@ -107,14 +118,14 @@ check_primary_secondary() {
     exec_on_node ${NODEA} "dd if=/dev/random of=${MNTTEST}/random count=20240"
     exec_on_node ${NODEA} "sha1sum  ${MNTTEST}/testing ${MNTTEST}/random > ${MNTTEST}/sha1sum"
     exec_on_node ${NODEA} "drbdadm status"
-    exec_on_node ${NODEA} "drbdadm dstate drbd"
+    exec_on_node ${NODEA} "drbdadm dstate drbdo2"
     echo "- Wait to get drbd sync"
     exec_on_node ${NODEA} "while (drbdadm status | grep Inconsistent); do sleep 5s; done"
     exec_on_node ${NODEA} "drbdadm status"
     exec_on_node ${NODEA} "umount ${MNTTEST}"
     echo "- Switch ${NODEA} to secondary"
-    exec_on_node ${NODEA} "drbdadm secondary drbd"
-    exec_on_node ${NODEB} "drbdadm primary drbd"
+    exec_on_node ${NODEA} "drbdadm secondary drbdo2"
+    exec_on_node ${NODEB} "drbdadm primary drbdo2"
     exec_on_node ${NODEB} "mount /dev/drbd0 ${MNTTEST}"
     exec_on_node ${NODEB} "cat ${MNTTEST}/sha1sum"
     exec_on_node ${NODEB} "sha1sum ${MNTTEST}/testing ${MNTTEST}/random > /mnt/testing_from_${NODEB}"
@@ -125,10 +136,10 @@ check_primary_secondary() {
 	echo "- Same Sha1 from ${NODEA} and ${NODEB}: TEST OK"
     fi
     echo "- Test pause/resume sync "
-    exec_on_node ${NODEB} "drbdadm pause-sync drbd"
+    exec_on_node ${NODEB} "drbdadm pause-sync drbdo2"
     exec_on_node ${NODEB} "dd if=/dev/zero of=${MNTTEST}/testing2 bs=1M count=24"
     exec_on_node ${NODEB} "drbdadm status"
-    exec_on_node ${NODEB} "drbdadm resume-sync drbd"
+    exec_on_node ${NODEB} "drbdadm resume-sync drbdo2"
     exec_on_node ${NODEB} "drbdadm status"
 }
 
@@ -137,11 +148,11 @@ back_to_begining() {
     umount_mnttest
     exec_on_node ${NODEA} "rm -rf ${MNTTEST}"
     exec_on_node ${NODEB} "rm -rf ${MNTTEST}"
-    exec_on_node ${NODEB} "drbdadm secondary drbd"
-    exec_on_node ${NODEA} "drbdadm down drbd"
-    exec_on_node ${NODEB} "drbdadm down drbd"
-    exec_on_node ${NODEA} "drbdadm disconnect drbd"
-    exec_on_node ${NODEA} "rm -vf /etc/drbd.d/drbd.res"
+    exec_on_node ${NODEB} "drbdadm secondary drbdo2"
+    exec_on_node ${NODEA} "drbdadm down drbdo2"
+    exec_on_node ${NODEB} "drbdadm down drbdo2"
+    exec_on_node ${NODEA} "drbdadm disconnect drbdo2"
+    exec_on_node ${NODEA} "rm -vf /etc/drbd.d/drbdo2.res"
     exec_on_node ${NODEA} "csync2 -xv"
 }
 
@@ -185,11 +196,8 @@ drbdconf_csync2
 #start_drbd
 finalize_DRBD_setup
 format_ocfs2
-echo "STOP HERE"
 
-echo "
-check_primary_secondary
-
+#check_primary_secondary
 #configure_resources
 
 # restore before runnning the test
@@ -200,6 +208,4 @@ disable_drbd
 # restore initial conf
 detach_disk_from_node
 delete_vol_drbd
-delete_pool_drbd DRBD"
-
-sleep 100
+delete_pool_drbd DRBD
