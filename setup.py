6764ce1a84f11b3f4268e7317f5f0313350ffe57
Kristoffer Grönlund <krig@koru.se>
2016-10-16 18:38:20 +0200

    Rename to hark

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from distutils.core import setup
+import os
+
+data_files = []
+data_files.append(('share/hark', ['hark.ini.example']))
+data_files.append(('share/hark/scenarios', ['scenarios/' + f for f in os.listdir('scenarios')]))
+data_files.append(('share/hark/templates', ['templates/' + f for f in os.listdir('templates')]))
+
+setup(name='Hark!',
+      version='0.1',
+      description='SLE HA VM creation / management tool',
+      author='Antoine Ginies + Kristoffer Gronlund',
+      author_email='aginies@suse.com, kgronlund@suse.com',
+      url='https://github.com/aginies/Deploy_HA_SLE_cluster.git',
+      scripts=['hark'],
+      data_files=data_files,
+      requires=['lxml', 'requests'])
